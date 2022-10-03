c54a1d83602fccf48342338c5e241bd5863d23b2
delseysilva <delseysilva@localhost>
2022-10-03 14:45:55 +0100

    Atributos e construtor

---
+++ src/Classes/Utente.java
@@ -0,0 +1,21 @@
+package Classes;
+
+public class Utente {
+	private String nome;
+	private String genero;
+	private String idade;
+	private String altura;
+	private String peso;
+	
+	public Utente(String nome, String genero, String idade, String altura, String peso) {
+		super();
+		this.nome = nome;
+		this.genero = genero;
+		this.idade = idade;
+		this.altura = altura;
+		this.peso = peso;
+	}
+	
+	
+	
+}
