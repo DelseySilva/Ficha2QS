c43805f597b529eb6b651560f43a3169b34649ef
delseysilva <delseysilva@localhost>
2022-10-03 14:52:27 +0100

    Getters e setters

--- src/Classes/Utente.java
+++ src/Classes/Utente.java
@@ -15,7 +15,29 @@ public class Utente {
 		this.altura = altura;
 		this.peso = peso;
 	}
-	
-	
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getAltura() {
+		return altura;
+	}
+
+	public void setAltura(String altura) {
+		this.altura = altura;
+	}
+
+	public String getPeso() {
+		return peso;
+	}
+
+	public void setPeso(String peso) {
+		this.peso = peso;
+	}
 	
 }
